45fa295f0e514ce60d9c840e721a14ff5a602ea4
Decagon <decagon@meggie-macBook-air.local>
2023-04-12 00:59:25 +0300

    frontend modified

--- order-ui/src/App.js
+++ order-ui/src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+
 import './App.css';
+
+import { AuthProvider } from './components/context/AuthContext';
+import { Routes,BrowserRouter as Router,Route, Navigate } from 'react-router-dom';
+import Home from './components/home/Home';
+import PrivateRoute from './components/misc/PrivateRoute';
+import Login from './components/home/Login';
+import Signup from './components/home/Signup';
+import Navbar from './components/misc/Navbar';
+import AdminPage from './components/admin/AdminPage';
+import UserPage from './components/user/UserPage';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthProvider>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path="/adminpage" element={<PrivateRoute><AdminPage /></PrivateRoute>}/>
+        <Route path="/userpage" element={<PrivateRoute><UserPage /></PrivateRoute>}/>
+        <Route path="*" element={<Navigate to="/" />}/>
+      </Routes>
+    </Router>
+  </AuthProvider>
   );
 }
 
